f6d52a2ce866ebb78648727b0b536a28e490b266
fukurou37 <swish3578@gmail.com>
2019-05-08 22:48:51 +0900

    途中経過

--- app/controllers/co_controller.rb
+++ app/controllers/co_controller.rb
@@ -1,18 +1,59 @@
 class CoController < ApplicationController
 
-  def home
-    @x = Kasitumon.all
-    @y = Kasitumon.new
-  end
+ def home1
+   @x = Kasitumon.all
 
-  def create
-    @z = Kasitumon.new(z_params)
-    @z.save
-    redirect_to "/kasitumon"
-  end
+   @x.each do |xx|
+       if xx.koumoku == "Day0" then
+         @y0 = xx.nitiji
+       elsif xx.koumoku == "Day1" then
+         @y1 = xx.nitiji
+       elsif xx.koumoku == "Day2" then
+         @y2 = xx.nitiji
+       elsif xx.koumoku == "Day3" then
+         @y3 = xx.nitiji
+       elsif xx.koumoku == "Day4" then
+         @y4 = xx.nitiji
+       end
+       @y = xx.nitiji
+   end
+ end
 
-  private
-  def z_params
-    params.require(:kasitumon).permit(:shitumonsya, :shitumonbun, :nitiji)
-  end
+ def home
+   @x = Kasitumon.all
+   @y = Kasitumon.new
+ end
+
+ def create
+   @z = Kasitumon.new(z_params)
+   @z.nitiji = Time.new
+   @z.save
+   redirect_to "/kasitumon"
+ end
+
+ def update
+   a = Kasitumon.new(z1_params)
+   @w = Kasitumon.find(a.id)
+
+   p params[:id]
+   if @w.nitiji1  == nil  then
+     @w.nitiji1 = Time.new
+   elsif @w.nitiji2 == nil  then
+     @w.nitiji2 = Time.new
+   elsif @w.nitiji3 == nil  then
+     @w.nitiji3 = Time.new
+   end
+
+   @w.update(z1_params)
+   redirect_to "/kasitumon"
+ end
+
+
+ private
+ def z_params
+   params.require(:kasitumon).permit(:shitumonsya, :shitumonbun)
+ end
+ def z1_params
+   params.require(:kasitumon).permit(:id , :hentousya1, :hentoubun1 , :hentousya2, :hentoubun2 , :hentousya3, :hentoubun3 )
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/kasitumon' => 'co#home'
-  post '/kasitumon' => 'co#create'
-  
+ get '/kasitumon' => 'co#home'
+
+
+ post '/kasitumon' => 'co#create'
+ patch '/kasitumon' => 'co#update'
+
+ get '/ka' => 'co#home1'
+
+ #get '/kasitumon/:id/edit' => 'co#edit'
+ #patch '/kasitumon/:id' => 'co#update'
+ #delete '/kasitumon/:id' => 'co#destroy'
 end
